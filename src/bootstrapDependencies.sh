e59edf0023a6e0e7d1766c7e903c855947865c50
scazan <scottcazan@gmail.com>
2022-01-12 11:18:00 +0100

    check for existing to avoid appearing as an error

--- src/bootstrapDependencies.sh
+++ src/bootstrapDependencies.sh
@@ -8,12 +8,18 @@ echo "adding ${NAMESPACE}/${REPO}..."
 # add the private repo root directory if it does not exist
 [ ! -d "node_modules/@${NAMESPACE}" ] && mkdir node_modules/@${NAMESPACE}
 
-if [ -z "$GIT_TOKEN" ]
+if [ ! -d "node_modules/@${NAMESPACE}/${REPO}" ]
 then
-  git clone --depth 1 --branch ${COMMIT} git@github.com:${NAMESPACE}/${REPO}.git ./node_modules/@${NAMESPACE}/${REPO}
+  if [ -z "$GIT_TOKEN" ]
+  then
+    git clone --depth 1 --branch ${COMMIT} git@github.com:${NAMESPACE}/${REPO}.git ./node_modules/@${NAMESPACE}/${REPO}
+  else
+    git clone --depth 1 --branch ${COMMIT} https://${GIT_TOKEN}:x-oauth-basic@github.com/${NAMESPACE}/${REPO}.git ./node_modules/@${NAMESPACE}/${REPO}
+  fi
+
+  echo "...added."
 else
-  git clone --depth 1 --branch ${COMMIT} https://${GIT_TOKEN}:x-oauth-basic@github.com/${NAMESPACE}/${REPO}.git ./node_modules/@${NAMESPACE}/${REPO}
+  echo "@${NAMESPACE}/${REPO} already exists. doing nothing"
 fi
 
-echo "...added."
 
