9dee71b76a9946e244e69940c2469ececd83fcf6
Tor Castensson <tor@plan8.se>
2023-06-01 15:30:16 +0200

    local path for monorepos

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
+const fs = require("fs");
+const { execSync } = require("child_process");
+const path = require("path");
 
-const packageJSON = fs.readFileSync('package.json');
+const packageJSON = fs.readFileSync("package.json");
 const config = JSON.parse(packageJSON);
 
 const dependencies = config.privateDependencies;
 
 if (dependencies) {
-  Object.entries(dependencies)
-    .forEach(keyVal => {
-      const [ dependency, branch ] = keyVal;
-      const [ namespace, repo ] = dependency.split('/');
+  Object.entries(dependencies).forEach((keyVal) => {
+    const [dependency, branch] = keyVal;
+    const [namespace, repo] = dependency.split("/");
 
-      const output = execSync(`node_modules/@plan8/bootstrap-private-repos/src/bootstrapDependencies.sh ${namespace.split('@').join('')} ${repo} ${branch}`);
-      console.log(output.toString());
-    });
+    const scriptPath = path.join(__dirname, "bootstrapDependencies.sh");
+
+    const output = execSync(
+      `${scriptPath} ${namespace.split("@").join("")} ${repo} ${branch}`
+    );
+
+    console.log(output.toString());
+  });
 }
